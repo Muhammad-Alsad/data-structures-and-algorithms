f2c0908c75c3038135bd0aa4e74f2026608594c4
muhammad <moalsad001@gmail.com>
2023-05-24 10:40:26 +0000

    linked-list-insertion v2

--- linked_list_insertions/linked-list-insertions.md
+++ linked_list_insertions/linked-list-insertions.md
@@ -1,1 +1,10 @@
 # code challange 06
+
+### append 
+![append function](./assets/append%20function.jpg)
+
+### insert-before
+![insert-before-function](./assets/insert%20before%20fun.jpg)
+
+### insert-after
+![insert-after-function](./assets/insert%20after%20fun.jpg)
--- linked_list_insertions/linked_list_insertions.py
+++ linked_list_insertions/linked_list_insertions.py
@@ -2,11 +2,18 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-
-
 class LinkedList:
     def __init__(self):
         self.head = None
+        
+    def to_string(self):
+        result = ""
+        current = self.head
+        while current is not None:
+            result += f"{{ {current.value} }} -> "
+            current = current.next
+        result += "NULL"
+        return result    
 
     def append(self, new_value):
         new_node = Node(new_value)
@@ -17,3 +24,36 @@ class LinkedList:
             while current.next:
                 current = current.next
             current.next = new_node
+    def insert_before(self, value, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            raise Exception("List is empty. Cannot insert before.")
+        if self.head.value == value:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        current = self.head
+        while current.next is not None:
+            if current.next.value == value:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
+        raise Exception("Value not found. Cannot insert before.")
+    
+    
+    def insert_after(self, value, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            raise Exception("List is empty. Cannot insert after.")
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
+        raise Exception("Value not found. Cannot insert after.")        
+
+if __name__ == "__main__":
+    pass    
