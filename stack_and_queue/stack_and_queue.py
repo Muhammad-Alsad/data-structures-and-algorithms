cf9059ba0b87ffebf902bd70ca7d3f06cd92e37c
muhammad <moalsad001@gmail.com>
2023-05-29 10:02:46 +0000

    v2

--- stack_and_queue/stack_and_queue.py
+++ stack_and_queue/stack_and_queue.py
@@ -9,6 +9,15 @@ class Stack:
         self.top = None
 
     def push(self, value):
+        """
+       Pushes a new node with the specified value onto the top of the stack.
+
+       Parameters:
+         - value: The value to be stored in the new node.
+
+        Returns:
+        None
+         """
         new_node = Node(value)
         if not self.top:
             self.top = new_node
@@ -17,6 +26,15 @@ class Stack:
             self.top = new_node
 
     def pop(self):
+        """
+           Removes and returns the value of the top node from the stack.
+
+          Raises:
+          - Exception: If the stack is empty.
+
+           Returns:
+           The value of the removed node.
+            """
         if not self.top:
             raise Exception("None")
         value = self.top.value
@@ -24,11 +42,21 @@ class Stack:
         return value
 
     def peek(self):
+        """
+        Returns the value of the top node from the stack without removing it.
+
+        Raises:
+        -  Exception: If the stack is empty.
+
+          Returns: 
+          The value of the top node.
+        """
         if not self.top:
             raise Exception("None")
         return self.top.value
 
     def is_empty(self):
+        """check if the stack is empty"""
         return not bool(self.top)
 
 
@@ -38,6 +66,15 @@ class Queue:
         self.rear = None
 
     def enqueue(self, value):
+        """
+               Adds a new node with the specified value to the rear of the queue.
+
+    Parameters:
+    - value: The value to be stored in the new node.
+
+    Returns:
+    None
+        """
         new_node = Node(value)
         if not self.front:
             self.front = new_node
@@ -47,6 +84,15 @@ class Queue:
             self.rear = new_node
 
     def dequeue(self):
+        """
+               Removes and returns the value of the front node from the queue.
+
+    Raises:
+    - Exception: If the queue is empty.
+
+    Returns:
+    The value of the removed node. 
+        """
         if not self.front:
             raise Exception("None")
         value = self.front.value
@@ -58,11 +104,23 @@ class Queue:
         return value
 
     def peek(self):
+        """
+            Returns the value of the front node from the queue without removing it.
+
+    Raises:
+    - Exception: If the queue is empty.
+
+    Returns:
+    The value of the front node.
+        """
         if not self.front:
             raise Exception("None")
         return self.front.value
 
     def is_empty(self):
+        """
+        check if queue is empty 
+        """
         return not bool(self.front)
 
 
