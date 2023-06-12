41d59dc6fe91531d401400f4df2dd610211cb6e2
muhammad <moalsad001@gmail.com>
2023-06-12 22:17:22 +0000

    add the white boarding

--- trees/tree.py
+++ trees/tree.py
@@ -79,9 +79,28 @@ class Tree:
 
         _walk(self.root)
         return output
+    
+    def find_maximum_value(self):
+        '''
+        find the max elemment in the binary tree and return it 
+        '''
+        def _find_maximum(root):
+            if root is None:
+                return float('-inf')
 
+            max_left = _find_maximum(root.left)
+            max_right = _find_maximum(root.right)
+
+            return max(max_left, max_right, root.value)
+
+        return _find_maximum(self.root)   
+    
 
 class BinarySearch(Tree):
+    def __init__(self, value=None):
+        super().__init__()
+        if value is not None:
+            self.add(value)
     def contains(self, value):
         try:
             is_found = False
@@ -122,8 +141,9 @@ class BinarySearch(Tree):
                     root.right = Tnode(value)
                     return
                 root = root.right
-    def tree_max():
-        pass                
+        
+         
+              
 
 if __name__ == "__main__":
     tree = Tree()
