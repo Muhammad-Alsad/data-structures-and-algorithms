7b85ba7503f649779ac36c698824d6e2e3686b75
muhammad <moalsad001@gmail.com>
2023-08-07 21:57:56 +0000

    Docstring

--- sorting/hashtable_left_join/hashtableleftjoin.py
+++ sorting/hashtable_left_join/hashtableleftjoin.py
@@ -1,13 +1,49 @@
 class Node:
+    """
+    A class representing a node in a linked list.
+
+    Each node contains a value and a reference to the next node in the linked list.
+
+    Attributes:
+        value: The value stored in the node.
+        next: A reference to the next node in the linked list. Default is None.
+
+
+    
+    """
     def __init__(self, value, _next=None):
         self.value = value
         self.next = _next
 
 
 class LinkedList:
+    """
+    A class representing a linked list data structure.
+
+    The linked list consists of nodes, each containing a value and a reference to the next node.
+
+    Attributes:
+        head: The first node in the linked list. Default is None.
+
+    Methods:
+        __init__(self, head=None):
+            Initializes a new LinkedList instance.
+        
+        insert(self, value):
+            Inserts a new node with the given value at the beginning of the linked list.
+    
+    
+    
+    """
     def __init__(self, head=None):
         self.head = head
+        """
+     Inserts a new node with the given value at the beginning of the linked list.
 
+        Args:
+            value: The value to be stored in the new node.
+
+    """
     def insert(self, value):
         new_node = Node(value)
         new_node.next = self.head
@@ -15,14 +51,49 @@ class LinkedList:
 
 
 class HashTable:
+    """
+         A class representing a basic hash table data structure.
+
+    The hash table stores key-value pairs using a hash function to map keys to indices.
+
+    Attributes:
+        size: The size of the hash table (number of buckets). Default is 1024.
+        buckets: A list of buckets to store key-value pairs. Each bucket may contain a linked list of pairs.
+        keys: A list to store all the keys present in the hash table.
+
+    Methods:
+        __init__(self, size=1024):
+            Initializes a new HashTable instance with the specified size.
+    
+    
+    """
     def __init__(self, size=1024):
         self.__size = size
         self.__buckets = [None] * size
         self.keys = []
 
+    """
+         Private method to generate a hash value for a given key.
+
+        Args:
+            key: The key for which to generate a hash value.
+
+        Returns:
+            The computed hash value.
+    
+    """
     def __hash(self, key):
        return sum([ord(char) for char in key]) * 283 % self.__size
     
+
+    """
+         Inserts a key-value pair into the hash table.
+
+        Args:
+            key: The key for the pair.
+            value: The value associated with the key. 
+    
+    """
     def set(self, key, value):
         index = self.__hash(key)
         if self.__buckets[index] is None:
@@ -30,6 +101,16 @@ class HashTable:
             self.__buckets[index] = ll
         self.__buckets[index].insert([key, value])
         self.keys.append(key)
+        """
+              Retrieves the value associated with a given key from the hash table.
+
+        Args:
+            key: The key for which to retrieve the value.
+
+        Returns:
+            The value associated with the key, or None if the key is not found.
+        
+        """
 
     def get(self, key):
         index = self.__hash(key)
@@ -42,13 +123,44 @@ class HashTable:
                 curr = curr.next
         return None
     
+    """
+       Checks if a given key exists in the hash table.
+
+        Args:
+            key: The key to check.
+
+        Returns:
+            True if the key exists, False otherwise.  
+    
+    """
     def has(self, key):
         if self.get(key):
             return True
         return False
+    """
+        Returns a list of all keys present in the hash table.
+
+        Returns:
+            A list of keys.
     
+    """
     def keys(self):
         return self.keys
+    """
+         Performs a left join operation on two hashmaps, combining their values based on matching keys.
+
+    This function takes two hashmaps as input and performs a left join operation on them.
+    It combines the values from hashmap1 and hashmap2 for matching keys and returns the result.
+
+    Args:
+        hashmap1: The first hashmap.
+        hashmap2: The second hashmap.
+
+    Returns:
+        A list of lists containing combined key-value pairs from hashmap1 and hashmap2.
+        Each inner list has the format [key, value_from_hashmap1, value_from_hashmap2].
+    
+    """
     
 def left_join(hashmap1, hashmap2):
         if not hashmap1:
